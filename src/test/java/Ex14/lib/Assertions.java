c62538a2d86a674d891d1e1e71d17621c674ad45
Mirka <igaleeva@rvision.ru>
2022-09-24 17:22:02 +0300

    testAuthUser() added

---
+++ src/test/java/Ex14/lib/Assertions.java
@@ -0,0 +1,15 @@
+package Ex14.lib;
+
+import static org.hamcrest.Matchers.hasKey;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.restassured.response.Response;
+
+public class Assertions {
+    public static void assertJsonByName(Response Response, String name, int expectedValue) {
+        Response.then().assertThat().body("$", hasKey(name));
+
+        int value = Response.jsonPath().getInt(name);
+        assertEquals(expectedValue, value, "JSON value is not equal to expected value");
+    }
+}
